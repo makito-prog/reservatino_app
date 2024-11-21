b8717496ad42ed87efa59a275f51310e686cd9fb
makito-prog <makito.soccer.28220305@gmail.com>
2024-11-21 11:52:49 +0900

    reservation昨日の追加

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,19 +1,46 @@
 class ReservationsController < ApplicationController
+  before_action :set_reservation, only: [:edit, :update, :destroy]
+
   def index
+    @reservations = Reservation.all
   end
 
   def new
+    @reservation = Reservation.new()
   end
 
   def create
+    @reservation = Reservation.new(reservation_params)
+  
+    if @reservation.save
+      redirect_to root_path, notice: "予約を作成しました。" # 保存成功時
+    else
+      render :new # 保存失敗時
+    end
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
+    if @reservation.update(reservation_params)
+      redirect_to reservations_path, notice: "予約を更新しました。"
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @reservation.destroy
+    redirect_to reservations_path, notice: "予約を削除しました。"
+  end
+
+  private
+
+  def set_reservation
+    @reservation = Reservation.find(params[:id])
+  end
+
+  def reservation_params
+    params.require(:reservation).permit(:date, :content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
-  get 'reservations/index'
-  get 'reservations/new'
-  get 'reservations/create'
-  get 'reservations/edit'
-  get 'reservations/update'
-  get 'reservations/destroy'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :reservations
+  root "reservations#index"
 end
